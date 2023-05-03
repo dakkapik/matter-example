a59dc895daede16808db242802a42d611fd23f07
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-05-03 13:43:20 -0400

    Update Input.js

--- Input.js
+++ Input.js
@@ -3,21 +3,21 @@ class Input {
         this.values = {
             play: true
         }
-        this.startButton = document.getElementById('play-gm');
+        // this.startButton = document.getElementById('play-gm');
         this.inputQueue = new Set();
 
-        this.startButton.addEventListener("click", ()=>{
-            if(!this.values.play){
-                Runner.run(runner, engine);
-            } else {
-                Runner.stop(runner);
-            }
+//         this.startButton.addEventListener("click", ()=>{
+//             if(!this.values.play){
+//                 Runner.run(runner, engine);
+//             } else {
+//                 Runner.stop(runner);
+//             }
 
-            this.values.play = !this.values.play;
-            this.values.play?
-            this.startButton.innerHTML = "stop":
-            this.startButton.innerHTML = "play";
-        })
+//             this.values.play = !this.values.play;
+//             this.values.play?
+//             this.startButton.innerHTML = "stop":
+//             this.startButton.innerHTML = "play";
+//         })
     }
 
     addKey(keyCode) {
