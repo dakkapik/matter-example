e5aeaa6ca35e691322f9a58dd09a427949008215
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-05-11 12:27:25 -0400

    attack with constraint, not working

--- Attack.js
+++ Attack.js
@@ -3,7 +3,7 @@ class Attack {
 
 
         this.options = {
-            isSensor : true,
+            // isSensor : true,
             label: "attack"
         }
         this.width = width;
@@ -24,7 +24,7 @@ class Attack {
         // console.log(this.bodyW)
         // console.log(centre.x - this.bodyW/2 - this.width/2)
 
-        let right = Bodies.rectangle(
+        let rightA = Bodies.rectangle(
             centre.x + this.bodyW/2 + this.width/2, 
             centre.y, 
             this.width, 
@@ -33,7 +33,7 @@ class Attack {
             );
 
 
-        let left = Bodies.rectangle(
+        let leftA = Bodies.rectangle(
             centre.x - this.bodyW/2 - this.width/2, 
             centre.y, 
             this.width, 
@@ -41,24 +41,31 @@ class Attack {
             this.options
             );
         
-            console.log(right)
-        let options = {
+            // console.log(right)
+        let constrOptionsR = {
             bodyA: this.body, 
-            bodyB: right,
+            bodyB: rightA,
             //change this to hypotenuse
-            // lenth: 1,
-            lenth: this.bodyW/2 + this.width/2,
-            stiffness: 0.8, 
+            lenth: 1,
+            // lenth: this.bodyW/2 + this.width/2,
+            stiffness: 1, 
         }
 
-        const constrR = Constraint.create(options)
+        let constrOptionsL = {
+            bodyA: this.body, 
+            bodyB: leftA,
+            //change this to hypotenuse
+            lenth: 1,
+            // lenth: this.bodyW/2 + this.width/2,
+            stiffness: 1, 
+        }
 
-        options.bodyB = left
+        const constrR = Constraint.create(constrOptionsR)
+        const constrL = Constraint.create(constrOptionsL)
 
-        const constrL = Constraint.create(options)
-
-        Composite.add(this.comp, [left, right, constrR, constrL])
-        return {r: right, l: left}
+        Composite.add(this.comp, [leftA, rightA, constrR, constrL])
+        // Composite.add(this.comp, [left, right])
+        return {r: rightA, l: leftA}
     }
 
 
@@ -88,7 +95,7 @@ class Attack {
         let pos = this.attBody.r.position;
         let angle = this.attBody.r.angle;
         push();
-        fill(this.color);
+        fill("red");
         translate(pos.x, pos.y);
         rotate(angle);
         
@@ -98,7 +105,7 @@ class Attack {
         pos = this.attBody.l.position;
         angle = this.attBody.l.angle;
         push();
-        fill(this.color);
+        fill("blue");
         translate(pos.x, pos.y);
         rotate(angle);
         
